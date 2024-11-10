5ca530aad3a0fe2559cee3a10b28767804523feb
okuoku <mjt@cltn.org>
2024-11-10 22:01:25 +0900

    gles2: Implement Section 3 updates for L2

--- include/yuniframe/cwgl.h
+++ include/yuniframe/cwgl.h
@@ -294,7 +294,7 @@ CWGL_API void cwgl_compressedTexSubImage2D(cwgl_ctx* ctx, cwgl_enum target, int3
 #ifdef CWGL_LEVEL_L2
 CWGL_API void cwgl_texStorage2D(cwgl_ctx* ctx, cwgl_enum target, int32_t levels, cwgl_enum internalformat, uint32_t width, uint32_t height);
 CWGL_API void cwgl_texStorage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t levels, cwgl_enum internalformat, uint32_t width, uint32_t height, uint32_t depth);
-CWGL_API void cwgl_texImage2D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, cwgl_enum internalformat, uint32_t width, uint32_t height, uint32_t depth, int32_t border, cwgl_enum format, cwgl_enum type, uint32_t offset);
+CWGL_API void cwgl_texImage2D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, cwgl_enum internalformat, uint32_t width, uint32_t height, int32_t border, cwgl_enum format, cwgl_enum type, uint32_t offset);
 CWGL_API void cwgl_texImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, cwgl_enum internalformat, uint32_t width, uint32_t height, uint32_t depth, int32_t border, cwgl_enum format, cwgl_enum type, const void* buf, size_t buflen);
 CWGL_API void cwgl_texImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, cwgl_enum internalformat, uint32_t width, uint32_t height, uint32_t depth, int32_t border, cwgl_enum format, cwgl_enum type, uint32_t offset);
 CWGL_API void cwgl_texSubImage2D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, int32_t xoffset, int32_t yoffset, uint32_t width, uint32_t height, cwgl_enum format, cwgl_enum type, uint32_t offset);
@@ -303,7 +303,7 @@ CWGL_API void cwgl_texSubImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t
 CWGL_API void cwgl_copyTexSubImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, int32_t xoffset, int32_t yoffset, int32_t zoffset, int32_t x, int32_t y, uint32_t width, uint32_t height);
 CWGL_API void cwgl_compressedTexImage2D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, cwgl_enum internalformat, uint32_t width, uint32_t height, int32_t border, uint32_t imagesize, uint32_t offset);
 CWGL_API void cwgl_compressedTexImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, cwgl_enum internalformat, uint32_t width, uint32_t height, uint32_t depth, int32_t border,uint32_t imagesize, const void* buf, size_t buflen);
-CWGL_API void cwgl_compressedTexImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, cwgl_enum internalformat, uint32_t width, uint32_t height, int32_t border, uint32_t imagesize, uint32_t offset);
+CWGL_API void cwgl_compressedTexImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, cwgl_enum internalformat, uint32_t width, uint32_t height, uint32_t depth, int32_t border, uint32_t imagesize, uint32_t offset);
 CWGL_API void cwgl_compressedTexSubImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, int32_t xoffset, int32_t yoffset, int32_t zoffset, uint32_t width, uint32_t height, uint32_t depth, uint32_t imagesize, cwgl_enum format, const void* buf, size_t buflen);
 CWGL_API void cwgl_compressedTexSubImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, int32_t xoffset, int32_t yoffset, int32_t zoffset, uint32_t width, uint32_t height, uint32_t depth, uint32_t imagesize, cwgl_enum format, uint32_t offset);
 #endif
--- src-gles2/cwgl-gles2-s3.c
+++ src-gles2/cwgl-gles2-s3.c
@@ -108,6 +108,163 @@ cwgl_compressedTexSubImage2D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, int
     CTX_LEAVE(ctx);
 }
 
+#ifdef CWGL_LEVEL_L2
+CWGL_API void 
+cwgl_texStorage2D(cwgl_ctx* ctx, cwgl_enum target, int32_t levels, 
+                  cwgl_enum internalformat, uint32_t width, uint32_t height){
+    CTX_ENTER(ctx);
+    glTexStorage2D(target, levels, internalformat, width, height);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_texStorage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t levels, 
+                  cwgl_enum internalformat, uint32_t width, uint32_t height,
+                  uint32_t depth){
+    CTX_ENTER(ctx);
+    glTexStorage3D(target, levels, internalformat, width, height, depth);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_texImage2D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                       cwgl_enum internalformat, uint32_t width, 
+                       uint32_t height, int32_t border, cwgl_enum format, 
+                       cwgl_enum type, uint32_t offset){
+    CTX_ENTER(ctx);
+    glTexImage2D(target, level, internalformat, width, height, border,
+                 format, type, (void*)(uintptr_t)offset);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_texImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                cwgl_enum internalformat, uint32_t width, uint32_t height, 
+                uint32_t depth, int32_t border, cwgl_enum format, 
+                cwgl_enum type, const void* buf, size_t buflen){
+    CTX_ENTER(ctx);
+    glTexImage3D(target, level, internalformat, width, height, depth,
+                 border, format, type, buf);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_texImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                       cwgl_enum internalformat, uint32_t width, 
+                       uint32_t height, uint32_t depth, int32_t border, 
+                       cwgl_enum format, cwgl_enum type, uint32_t offset){
+    CTX_ENTER(ctx);
+    glTexImage3D(target, level, internalformat, width, height, depth,
+                 border, format, type, (void*)(uintptr_t)offset);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_texSubImage2D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                          int32_t xoffset, int32_t yoffset, uint32_t width, 
+                          uint32_t height, cwgl_enum format, cwgl_enum type, 
+                          uint32_t offset){
+    CTX_ENTER(ctx);
+    glTexSubImage2D(target, level, xoffset, yoffset, width, height, format, 
+                    type, (void*)(uintptr_t)offset);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_texSubImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                   int32_t xoffset, int32_t yoffset, int32_t zoffset, 
+                   uint32_t width, uint32_t height, uint32_t depth, 
+                   cwgl_enum format, cwgl_enum type, const void* buf, 
+                   size_t buflen){
+    CTX_ENTER(ctx);
+    glTexSubImage3D(target, level, xoffset, yoffset, zoffset, width, height,
+                    depth, format, type, buf);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_texSubImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                          int32_t xoffset, int32_t yoffset, int32_t zoffset, 
+                          uint32_t width, uint32_t height, uint32_t depth, 
+                          cwgl_enum format, cwgl_enum type, uint32_t offset){
+    CTX_ENTER(ctx);
+    glTexSubImage3D(target,level, xoffset, yoffset, zoffset, width, height,
+                    depth, format, type, (void*)(uintptr_t)offset);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_copyTexSubImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                       int32_t xoffset, int32_t yoffset, int32_t zoffset, 
+                       int32_t x, int32_t y, uint32_t width, uint32_t height){
+    CTX_ENTER(ctx);
+    glCopyTexSubImage3D(target, level, xoffset, yoffset, zoffset, x, y, 
+                        width, height);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_compressedTexImage2D_buffer(cwgl_ctx* ctx, cwgl_enum target, 
+                                 int32_t level, cwgl_enum internalformat, 
+                                 uint32_t width, uint32_t height, 
+                                 int32_t border, uint32_t imagesize, 
+                                 uint32_t offset){
+    CTX_ENTER(ctx);
+    glCompressedTexImage2D(target, level, internalformat, width, height,
+                           border, imagesize, (void*)(uintptr_t)offset);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_compressedTexImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                          cwgl_enum internalformat, uint32_t width, 
+                          uint32_t height, uint32_t depth, int32_t border,
+                          uint32_t imagesize, const void* buf, size_t buflen){
+    CTX_ENTER(ctx);
+    glCompressedTexImage3D(target, level, internalformat, width, height,
+                           depth, border, imagesize, buf);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_compressedTexImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, 
+                                 int32_t level, cwgl_enum internalformat, 
+                                 uint32_t width, uint32_t height, 
+                                 uint32_t depth, int32_t border, 
+                                 uint32_t imagesize, uint32_t offset){
+    CTX_ENTER(ctx);
+    glCompressedTexImage3D(target, level, internalformat, width, height,
+                           depth, border, imagesize, (void*)(uintptr_t)offset);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_compressedTexSubImage3D(cwgl_ctx* ctx, cwgl_enum target, int32_t level, 
+                             int32_t xoffset, int32_t yoffset, int32_t zoffset,
+                             uint32_t width, uint32_t height, uint32_t depth, 
+                             uint32_t imagesize, cwgl_enum format, 
+                             const void* buf, size_t buflen){
+    CTX_ENTER(ctx);
+    glCompressedTexSubImage3D(target, level, xoffset, yoffset, zoffset,
+                              width, height, depth, imagesize, format, buf);
+    CTX_LEAVE(ctx);
+}
+
+CWGL_API void 
+cwgl_compressedTexSubImage3D_buffer(cwgl_ctx* ctx, cwgl_enum target, 
+                                    int32_t level, int32_t xoffset, 
+                                    int32_t yoffset, int32_t zoffset, 
+                                    uint32_t width, uint32_t height, 
+                                    uint32_t depth, uint32_t imagesize, 
+                                    cwgl_enum format, uint32_t offset){
+    CTX_ENTER(ctx);
+    glCompressedTexSubImage3D(target, level, xoffset, yoffset, zoffset, width,
+                              height, depth, imagesize, format,
+                              (void*)(uintptr_t)offset);
+    CTX_LEAVE(ctx);
+}
+#endif
+
 // 3.7.4 Texture Parameters
 CWGL_API void 
 cwgl_texParameterf(cwgl_ctx* ctx, cwgl_enum target, cwgl_enum pname, float param){
